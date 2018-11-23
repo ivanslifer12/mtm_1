c11b99c39fec8b0009086cecfd4be0162acaf1d3
Bar Raveh <baraveh@gmail.com>
2018-11-23 16:28:48 +0200

    potato

--- uniqueOrderedList/uniqueOrderedList.c
+++ uniqueOrderedList/uniqueOrderedList.c
@@ -7,9 +7,44 @@
 #include <stdlib.h>
 #include <assert.h>
 
-struct uniqueOrderedList_t
-{
+struct uniqueOrderedList_t {
     Element data;
     UniqueOrderedList next;
+    copyElements copyNode;
+    freeElements freeNode;
+    elementsEquals equalNode;
+    elementGreaterThan greaterNode;
 };
+
+//Unique ordered list constructor
+UniqueOrderedList uniqueOrderedListCreate(copyElements copyNode, freeElements freeNode,
+        elementsEquals equalsNode, elementGreaterThan greaterNode) {
+  //Checking none of the functions given are null
+    if( !copyNode || !freeNode || !equalsNode || !greaterNode) {
+      return NULL;
+  }
+
+  UniqueOrderedList listPointer = malloc(sizeof(*listPointer));
+    //Checking Malloc
+  if(listPointer == NULL) {
+      return NULL;
+  }
+
+  //allocating space for data
+  listPointer-> data = malloc(sizeof(Element));
+  if(listPointer-> data == NULL){
+      free(listPointer);
+      return NULL;
+  }
+
+  //allocating space for pointer to next element
+  listPointer -> next = malloc(sizeof(Element*));
+    if(listPointer-> next == NULL){
+        free(listPointer);
+        return NULL;
+    }
+
+
+}
+
 
