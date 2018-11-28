82ae991cdb8fe624351b42659152387e4c441e53
Bar Raveh <baraveh@gmail.com>
2018-11-28 19:51:41 +0200

    tomato

---
+++ citizen.c
@@ -0,0 +1,5 @@
+//
+// Created by Bar The magical on 28/11/2018.
+//
+
+#include "citizen.h"
---
+++ citizen.h
@@ -0,0 +1,20 @@
+//
+// Created by Bar The magical on 28/11/2018.
+//
+
+#ifndef MTM_1_CITIZEN_H
+#define MTM_1_CITIZEN_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct citizen_t* Citizen;
+
+/** citizen maker
+ * this function assumes all parameters are ok, since they are
+ * checked within mtm elections anyways in order to know the return value*/
+Citizen citizenCreate (string citizenName, )
+
+
+#endif //MTM_1_CITIZEN_H
