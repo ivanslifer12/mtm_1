9a4eca2fb48ec972006432a939b727abee37f197
Bar Raveh <baraveh@gmail.com>
2018-11-29 12:13:15 +0200

    citizen.h

--- citizen.h
+++ citizen.h
@@ -13,8 +13,27 @@ typedef struct citizen_t* Citizen;
 
 /** citizen maker
  * this function assumes all parameters are ok, since they are
- * checked within mtm elections anyways in order to know the return value*/
-Citizen citizenCreate (string citizenName, )
+ * checked within mtm elections anyways in order to know the return value
+ * the only error can be a memory error */
+Citizen citizenCreate (const char* citizenName, int citizenId, int citizenAge, int yearsOfEducation, int cityId);
+
+/** return a copy of the citizen's name through the pointer it gets*/
+/** once again, this function assumes the citizen it gets is valid and has a name */
+/** so the only error can be a memeory error */
+void getName (Citizen citizenToGet, char** namePtr);
+
+/** similar to getName, only returns the city */
+void getCity (Citizen citizenToGet, int* cityIdPtr);
+
+void getAge (Citizen citizenToGet, int* agePtr);
+
+void getYearsOfEducation (Citizen citizenToGet, int* yearOfEducationPtr);
+
+/** makes a citizen a candidate */
+void makeCandidate (Citizen candidate);
+
+/** deletes a citizen */
+void citizenDestroy (Citizen citizenToDestroy);
 
 
 #endif //MTM_1_CITIZEN_H
